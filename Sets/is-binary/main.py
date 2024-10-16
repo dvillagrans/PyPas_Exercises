91622e2c064aebf8a8a5294f2ebc91984172686b
Diego Villagran <diegovillasal@gmail.com>
2024-10-15 20:17:01 -0600

    todos los de sets

---
+++ Sets/is-binary/main.py
@@ -0,0 +1,16 @@
+def run(input_str: str) -> bool:
+    # Verificar si la cadena solo contiene caracteres '0' y '1'
+    return set(input_str).issubset({'0', '1'})
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(run)
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import vendor
+
+    vendor.launch(run)
